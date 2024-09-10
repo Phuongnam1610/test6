949bb11f32dc785da8501ee499c68a891cbc60bb
Manhq <manhq032@gmail.com>
2024-09-10 09:45:46 +0700

    sửa bth5

--- BTH5/tailieu.py
+++
@@ -1,5 +0,0 @@
-class tailieu:
-    def __init__(self, ten,  id):
-        self.ten = ten
-        self.id=id
-        self.body=""
--- BTH5/test5.py
+++
@@ -1,3 +0,0 @@
-from config import get_setting,update_setting
-input_status=get_setting('SAVED')
-print(input_status)
